448d0c6bd8d014952eea46f8245a99bf4fd1de3f
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-01-10 20:18:03 +0600

    8

---
+++ 08/8.js
@@ -0,0 +1,25 @@
+const { readFileSync } = require('node:fs')
+const { basename } = require('node:path')
+
+const day = basename(__filename, '.js')
+const input = readFileSync(`${day}.txt`, 'utf8').trim().split('\n')
+
+const n = input.length
+
+for (let part2 of [false,true]) {
+  let ans = 0
+
+  for (let line of input) {
+    const original = line
+    const decode = eval(line)
+    const encode = line
+      .replaceAll(/\\/gi, String.raw`\\`)
+      .replaceAll(/"/gi, String.raw`\"`)
+
+    ans += part2
+      ? encode.length - original.length + 2
+      : original.length - decode.length
+  }
+
+  console.log(ans);
+}
--- getInput.js
+++ getInput.js
@@ -1,14 +1,16 @@
 const { argv } = require('node:process')
 const { execSync } = require('node:child_process')
-const { writeFileSync, copyFileSync } = require('node:fs')
+const { writeFileSync, copyFileSync, mkdirSync } = require('node:fs')
 
 const day = argv[2]
-const cookie = 'session=53616c7465645f5f1270f005cb04451d01dffbe050a263e428b88170204cdc19359d1f3e9310822200df607df6efb7c2912bd4099ddadf4d4e3e6c9957b4e379'
+const cookie = 'session=53616c7465645f5f96251f8097e97db0d54aeefa84c9a077171688b26f6aee136ecc36f3750ddfa606fb94e3f4002a8709c98167bebd837664b4882ac8308e17'
 
 ;(async () => {
   const res = await fetch(`https://adventofcode.com/2015/day/${day}/input`, { headers: { cookie } })
   const input = await res.text()
   const pday = day.padStart(2, 0)
+
+  mkdirSync(pday)
 
   writeFileSync(`${pday}/${day}.txt`, input)
   console.log(`${day}.txt written`);
